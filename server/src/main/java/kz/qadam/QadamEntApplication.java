7b1e65c75a225c5b229309ee7e6caac03aa57374
Seideraly Sayat <136960142+seideraly02@users.noreply.github.com>
2026-08-06 21:47:18 +0500

    feat(backend): add Spring Boot entry point

---
+++ server/src/main/java/kz/qadam/QadamEntApplication.java
@@ -0,0 +1,11 @@
+package kz.qadam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QadamEntApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QadamEntApplication.class, args);
+    }
+}
