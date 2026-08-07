a5e3ffdecc5df75822649e24270d08f2c44e9aeb
Seideraly Sayat <136960142+seideraly02@users.noreply.github.com>
2026-08-07 06:55:39 +0500

    refactor(backend): enable typed configuration

--- server/src/main/java/kz/qadam/QadamEntApplication.java
+++ server/src/main/java/kz/qadam/QadamEntApplication.java
@@ -2,8 +2,10 @@ package kz.qadam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class QadamEntApplication {
     public static void main(String[] args) {
         SpringApplication.run(QadamEntApplication.class, args);
